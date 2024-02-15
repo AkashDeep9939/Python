c95582933699d6fc14bda749c6e4355be603b57a
Akash Deep <akashdeep9939@gmail.com>
2024-02-15 17:28:09 +0000

    Basic question in for loop python

---
+++ 03_loops/01_solution.py
@@ -0,0 +1,8 @@
+numbers = [1, -2, 3, -4, 5, 6, -7, 9, 10]
+
+positive_number_count = 0
+
+for it in numbers:
+    if it > 0:
+        positive_number_count += 1
+print("final cout of positive number is :",positive_number_count)
